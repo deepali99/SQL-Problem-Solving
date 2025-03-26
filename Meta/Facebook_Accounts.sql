778d18b7afa20744fdc432463bf1af964aba555e
Deepali Sharma <35188717+deepali99@users.noreply.github.com>
2025-03-26 14:36:08 -0700

    Create Facebook_Accounts.sql

---
+++ Meta/Facebook_Accounts.sql
@@ -0,0 +1,9 @@
+/*
+  https://platform.stratascratch.com/coding/10296-facebook-accounts?code_type=3
+  Calculate the ratio of accounts closed on January 10th, 2020 using the fb_account_status table.
+  */
+  
+  SELECT 
+  COUNT(CASE WHEN status = 'closed' THEN 1 END) / COUNT(*) AS ratio
+  FROM fb_account_status
+  WHERE date = '2020-01-10'
