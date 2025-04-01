cccb3b3a67687c8ce74e4c6f73689cde9329aa8d
Deepali Sharma <35188717+deepali99@users.noreply.github.com>
2025-04-01 11:40:06 -0700

    Create Customer_Feedback_Analysis.sql

---
+++ Customer_Feedback_Analysis.sql
@@ -0,0 +1,15 @@
+/*
+https://platform.stratascratch.com/coding/10366-customer-feedback-analysis?code_type=3
+Capital One's marketing team is working on a project to analyze customer feedback from their feedback surveys.
+The team sorted the words from the feedback into three different categories;
+•	short_comments
+•	mid_length_comments
+•	long_comments
+The team wants to find comments that are not short and that come from social media. The output should include 'feedback_id,' 'feedback_text,' 'source_channel,' and a calculated category.
+*/
+
+SELECT DISTINCT feedback_id,feedback_text,source_channel,comment_category
+FROM customer_feedback
+WHERE comment_category<>'short_comments'
+AND 
+source_channel = 'social_media';
