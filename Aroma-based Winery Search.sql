c9c05ba9fbb62ab19752a986a674b0f951722371
Deepali Sharma <35188717+deepali99@users.noreply.github.com>
2025-05-01 16:31:49 -0700

    Create Aroma-based Winery Search.sql

---
+++ Aroma-based Winery Search.sql
@@ -0,0 +1,9 @@
+/*
+https://platform.stratascratch.com/coding/10026-find-all-wineries-which-produce-wines-by-possessing-aromas-of-plum-cherry-rose-or-hazelnut?code_type=3
+Find wineries producing wines with aromas of plum, cherry, rose, or hazelnut (singular form only). Substring matches, like cherries should be excluded.
+*/
+
+
+SELECT DISTINCT winery FROM winemag_p1
+WHERE lower(description) 
+REGEXP '(plum|cherry|rose|hazelnut)([^a-z])' 
